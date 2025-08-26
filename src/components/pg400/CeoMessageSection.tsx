ccb2fbc05b98e9cbd7f48b182dfe0ad956e329ee
brynhild <kiritsukiyena@gmail.com>
2025-08-26 17:21:40 +0900

    fix css

--- src/components/pg400/CeoMessageSection.tsx
+++ src/components/pg400/CeoMessageSection.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
-
 import styles from './CeoMessageSection.module.css';
 
-// 1. Define the Props type for the component
 type CeoMessageSectionProps = {
   title: string;
   paragraph1: string;
@@ -11,25 +9,22 @@ type CeoMessageSectionProps = {
   signature: string;
 };
 
-// 2. Receive all data from props
 const CeoMessageSection = ({ title, paragraph1, paragraph2, signature }: CeoMessageSectionProps) => {
     return (
         <section id="ceo-message" className={styles.section}>
             <div className={styles.container}>
                 <div className={styles.imageWrapper}>
-                    {/* 3. Use image data passed from props */}
-                    {/* <Image 
-                        src={imageSrc}
-                        alt={imageAlt} 
-                        width={200} 
-                        height={240} 
-                        className={styles.ceoImage} 
-                    /> */}
+
+                    <Image 
+                      src="/images/ceo-portrait.jpg" // Replace with the actual image path
+                      alt="Portrait of the CEO" 
+                      width={400} 
+                      height={500} 
+                      className={styles.ceoImage}
+                    />
                 </div>
                 <div className={styles.textWrapper}>
-                    {/* 4. Render title with HTML tags using dangerouslySetInnerHTML */}
                     <h2 className={styles.title} dangerouslySetInnerHTML={{ __html: title }} />
-                    {/* 5. Use paragraphs and signature from props */}
                     <p className={styles.paragraph}>
                         {paragraph1}
                     </p>
